8d62b1b8399342def554aefbe2f07369dbb7c54b
Gaspar <gaspar.tan1189@gmail.com>
2023-11-26 01:33:02 -0800

    copilot codespace

--- pdfchat/app/components/uploadbox.tsx
+++ pdfchat/app/components/uploadbox.tsx
@@ -2,12 +2,12 @@ import {BiUpload} from 'react-icons/bi'
 
 export default function UploadBox() {
     return (
-        <div className="bg-beige flex align-center justify-center border border-blue-500">
+        <div className="bg-beige flex align-center justify-center border border-blue-600">
             <div>
                 <BiUpload size={70}> </BiUpload>
             </div>
             <div>
-                
+                <p className='justify-center'> Upload Your PDF Here </p>
             </div>
         </div>
     )
